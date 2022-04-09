d8263c2e24658150e69c8f9cfb7790114fa970f0
Pierry <derungspierry15@gmail.com>
2022-04-09 11:27:45 -0300

    AAAAA

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+ <html>
+   <head>
+    <title>Primeiro php</title>
+    </head>
+   <body>
+
+  <?php
+  $nome = "Pierry";
+   echo "ola, meu nome é $nome" ;
+  ?>
+
+  </body>
+</html>
